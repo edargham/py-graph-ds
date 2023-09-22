3c9f994f41a533ebf6da8d26171e4d09dc7a5252
Elias Dargham <edargham@hotmail.com>
2023-09-22 20:12:39 +0300

    Elias Dargham 22/09/2023 - BFS and DFS Implementations.

--- graph/graph.py
+++ graph/graph.py
@@ -38,33 +38,116 @@ class Graph():
 
     # def _bfs_get_neigbors()
 
+    def _find_neighbors(self, v_idx: int) -> np.ndarray:
+        nbs: list[int] = []
+
+        if (v_idx > self.adj_mat.shape[0]):
+            raise Exception('Index specified exeeds graph width')
+        # endif
+
+        for i in range(self.adj_mat[v_idx].shape[0]):
+            if self.adj_mat[i] != 0:
+                nbs.append(i)
+            # endif
+        # endfor
+
+        return np.array(nbs)
+    # endmethod
+
+    def _is_neighbor(self, fringe: np.ndarray) -> int:
+        for i in range(fringe.size):
+            if fringe[i] != 0 and self.vertices[i].get_mark() == 0:
+                return i
+            # endif
+        # endfor
+        return None
+    # endmethod
+
     def bfs(self, initial_node_idx:int=0, callback=None):
-        # Initialize queue and set enqueue the initial node's index.
         bfs_queue = []
         bfs_queue.append(initial_node_idx)
 
-        if (type(out:=callback(self.vertices[initial_node_idx])) != NoneType):
+        if (callback is not None and type(out:=callback(self.vertices[initial_node_idx])) != NoneType):
             self.vertices[initial_node_idx].set_mark(1)
             return out
         # endif
 
         self.vertices[initial_node_idx].set_mark(1)
 
-        while bfs_queue != 0:
+        while bfs_queue:
             idx: int = bfs_queue.pop(0)
+            neighs: list[int] = self._find_neighbors(idx)
 
+            for n in neighs:
+                if self.vertices[n].get_mark() == 0:
+                    bfs_queue.append(n)
+                    
+                    if (callback is not None and type(out:=callback(self.vertices[n])) != NoneType):
+                        self.vertices[n].set_mark(1)
+                        return self.vertices[n], out
+                    # endif
 
+                    self.vertices[n].set_mark(1)
+                # endif
+            # endfor
         # endwhile
 
-        return
+        return None
     # endmethod
 
     def bfs_all(self, callback=None):
-        return
+        for vtx in self.vertices:
+            vtx.set_mark(0)
+        # endfor
+
+        last = None
+
+        for i in range(self.vertices.shape[0]):
+            last = self.bfs(i, callback)
+        # endfor
+        
+        return last
     # endmethod
 
-    def dfs(self, callback=None):
-        return
+    def dfs(self, initial_node_idx=0, pre_callback=None, post_callback=None):
+        dfs_stack=[]
+        dfs_stack.append(initial_node_idx)
+        
+        self.vertices[initial_node_idx].set_mark(1)
+
+        while dfs_stack:
+            current_idx = dfs_stack[0]
+            next_idx = self._is_neighbor(self.adj_mat[current_idx])
+
+            if next_idx is not None:
+                self.vertices[next_idx].set_mark(1)
+                dfs_stack.insert(0, next_idx)
+
+                if (pre_callback is not None and type(pre_out:=pre_callback(self.vertices[next_idx])) != NoneType):
+                    return self.vertices[next_idx], pre_out
+                # endif
+            # endif
+            else:
+                popped_idx = dfs_stack.pop(0)
+                if (post_callback is not None and type(post_out:=post_callback(self.vertices[popped_idx])) != NoneType):
+                    return self.vertices[popped_idx], post_out
+                # endif
+            # endelse
+        # endwhile
+    # endmethod
+
+    def dfs_all(self, pre_callback=None, post_callback=None):
+        for vtx in self.vertices:
+            vtx.set_mark(0)
+        # endfor
+
+        last = None
+
+        for i in range(self.vertices.shape[0]):
+            last = self.dfs(i, pre_callback, post_callback)
+        # endfor
+
+        return last
     # endmethod
 
     def ucs(self):
